ea50c705ca60e164e6231a145a48798362a05b63
Engelbrecht <patrik.engelbrecht@edu.bme.hu>
2023-10-11 09:39:10 +0200

    secure

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.spaceship;
 
+import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -8,9 +9,11 @@ import java.util.Random;
 * (Deliberately contains bugs.)
 */
 public class TorpedoStore {
-  private Random random = new Random(); 
+  private SecureRandom random = new SecureRandom(); 
+  
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
+
 
   private int torpedoCount = 0;
 
